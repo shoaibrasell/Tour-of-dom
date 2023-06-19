7ca7c8b7b572f74a3330f9c39b8fc9bfc5ae8bb4
Shoaib Ullah Rasel <56650099+shoaibrasell@users.noreply.github.com>
2023-06-19 14:39:07 +0600

    Add files via upload
    practice dom

---
+++ Tour-of-dom/js/append.js
@@ -0,0 +1,16 @@
+// 1. whete to addc 
+const placeList = document.getElementById('places-list');
+// 2. what to be added
+const li = document.createElement('li');
+li.innerText = "NEW York ";
+
+// 3. add the child
+placeList.appendChild(li)
+
+// 1. Where to be added
+const mainContainer = document.getAnimations('main-container');
+
+// 2.What to be added
+const h1 = document.createElement('main-container');
+
+
